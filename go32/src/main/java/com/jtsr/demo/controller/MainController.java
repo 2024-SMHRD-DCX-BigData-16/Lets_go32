8d0b8c7875e2b2dc2307e49bc82d0773ba5a8c56
yj <dfyd15937@gmail.com>
2025-02-20 11:20:37 +0900

    전체적인세팅

---
+++ go32/src/main/java/com/jtsr/demo/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.jtsr.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String main() {
+		return "NewFile";
+	}
+	
+}
